c14973285c999b900d3573fc4676e0ac366c6816
Mark <stauber@stanford.edu>
2018-07-08 20:31:43 -0400

    support unique IDs

--- Assets/Helpers/SceneControl.cs
+++ Assets/Helpers/SceneControl.cs
@@ -12,7 +12,7 @@ using UnityEngine.SceneManagement;
 public class SceneControl : MonoBehaviour {
 
     public List<GameObject> prefabs;
-	public List<GameObject> assetsToSave;
+    private List<GameObject> sceneAssets = new List<GameObject>();
 
 	public GameObject saveAssetButton;
 	public Text notification;
@@ -21,14 +21,13 @@ public class SceneControl : MonoBehaviour {
 
 	private MapSession mapSession;
 	private FocusSquare focusSquare;
-	private List<MapAsset> loadedAssets = new List<MapAsset>();
-	private List<GameObject> instantiatedAssets = new List<GameObject>();
 
     public GameObject progressPanel;
     private int progress = 0;
 
 
 	void Start(){
+        
 		//Set up references
 		mapSession = GameObject.Find("MapSession").GetComponent<MapSession> ();
 		focusSquare = GameObject.Find("FocusSquare").GetComponent<FocusSquare> ();
@@ -37,10 +36,6 @@ public class SceneControl : MonoBehaviour {
 		bool isMappingMode = PlayerPrefs.GetInt ("IsMappingMode") == 1;
 		string mapID = PlayerPrefs.GetString ("MapID");
 		string userID = PlayerPrefs.GetString ("UserID");
-
-		foreach (var asset in instantiatedAssets) {
-			asset.SetActive (false);
-		}
 
 		mapSession.Init (isMappingMode ? MapMode.MapModeMapping : MapMode.MapModeLocalization, userID, mapID);
 
@@ -52,39 +47,16 @@ public class SceneControl : MonoBehaviour {
 		//Set callback that confirms when assets are stored
 		mapSession.AssetStoredEvent += stored => {
 			Debug.Log ("Assets stored: " + stored);
-			Toast("Your bear's garden has been planted!", 2.0f);
 		};
 
-		//Set Callback for when assets are reloaded
-		mapSession.AssetLoadedEvent += mapAsset => {
-			if(IsThisALoadedAsset(mapAsset)) {
-				Debug.Log(mapAsset.AssetId + " already loaded");
-				return;
-			}
-
-			Vector3 position = new Vector3 (mapAsset.X, mapAsset.Y, mapAsset.Z);
-			Quaternion orientation = Quaternion.Euler(0, mapAsset.Orientation, 0);
-			Debug.Log(mapAsset.AssetId + " found at: " + position.ToString());
-			GameObject instantiatedAsset = Instantiate(GetPrefab(mapAsset.AssetId), position, orientation);
-			instantiatedAssets.Add(instantiatedAsset);
-
-			Toast("Your bear's garden has been found!", 2.0f);
-
-			placeAssetButtons.SetActive(true);
-			saveAssetButton.SetActive (true);
-			loadedAssets.Add(mapAsset);
-		};
+        //Set Callback for when assets are reloaded
+        mapSession.AssetLoadedEvent += LoadAsset;
 
 		mapSession.ObjectDetectedEvent += detectedObject => {
             Debug.Log("Detected " + detectedObject.Name);
 		};
 
         mapSession.ProgressIncrementedEvent += ProgressIncrement;
-
-        mapSession.ProgressIncrementedEvent += progress =>
-        {
-            Debug.Log("In Progress " + progress);
-        };
 
 		//Set up the UI of the scene
 		saveAssetButton.SetActive (false);
@@ -95,16 +67,6 @@ public class SceneControl : MonoBehaviour {
 		} 
 
 		Toast ("First scan around your area to start!", 10.0f);
-	}
-
-	private bool IsThisALoadedAsset(MapAsset asset){
-		foreach(var loaded in loadedAssets) {
-			if (asset.AssetId == loaded.AssetId && (asset.Position - loaded.Position).magnitude < 1e-4) {
-				return true;
-			}
-		}
-			
-		return false;
 	}
 
 	void Update(){
@@ -124,7 +86,6 @@ public class SceneControl : MonoBehaviour {
 	public void PlaceAsset(String assetName) { 
 		//Only place asset if focused on a plane
 		if (focusSquare.SquareState != FocusSquare.FocusState.Found) {
-			Debug.Log ("Focus square hasn't been found yet");
 			Toast ("Point to a surface to place plants.", 2.0f);
 			return;
 		}
@@ -132,46 +93,50 @@ public class SceneControl : MonoBehaviour {
 		//Instantiate prefab on focus square
 		Vector3 position = focusSquare.foundSquare.transform.position;
 		GameObject asset = Instantiate(GetPrefab(assetName), position, Quaternion.identity);
-		assetsToSave.Add (asset);
+        asset.name = assetName + "," + UnityEngine.Random.Range(0, 10000).ToString();
+        sceneAssets.Add(asset);
 
-        SaveAsset();
+        SaveAssets();
 
 	}
 
 	//Saving assets using the MapSession
-	public void SaveAsset() {
+    public void SaveAssets() {
+        List<MapAsset> mapAssets = new List<MapAsset>();
+        foreach(GameObject asset in sceneAssets){
+            MapAsset mapAsset = new MapAsset(asset.name, asset.transform.rotation.y, asset.transform.position);
+            mapAssets.Add(mapAsset);
+        }
 
-		List<MapAsset> mapAssetsToSave = new List<MapAsset> ();
+        mapSession.StorePlacements (mapAssets);
 
-		foreach (GameObject asset in assetsToSave) {
-			MapAsset mapAsset = new MapAsset (asset.name, asset.transform.rotation.y, asset.transform.position);
-			mapAssetsToSave.Add (mapAsset);
-			Debug.Log ("Asset stored at: " + asset.transform.position.ToString ());
-		}
-
-		mapSession.StorePlacements (mapAssetsToSave);
-
-		Toast ("Please wait while your bear's garden is planted.", 10.0f);
 	}
 
-	public void Back(){
-		SceneManager.LoadSceneAsync ("LoginScene");
-	}
+    public void LoadAsset(MapAsset mapAsset){
+        GameObject isLoaded = IsThisALoadedAsset(mapAsset);
+        if (isLoaded != null)
+        {
+            isLoaded.transform.position = mapAsset.Position;
+            isLoaded.transform.rotation = Quaternion.Euler(0, mapAsset.Orientation, 0);
+            return;
+        }
 
-	private GameObject GetPrefab(string name) {
-		Debug.Log ("Looking for a " + name);
-		foreach(GameObject prefab in prefabs ){
-			if(prefab.name == name){
-				return prefab;
-			} else if (name.Length > 7 && prefab.name == name.Substring(0, name.Length - 7)) {
-				return prefab;
-			}
-		}
+        Vector3 position = new Vector3(mapAsset.X, mapAsset.Y, mapAsset.Z);
+        Quaternion orientation = Quaternion.Euler(0, mapAsset.Orientation, 0);
+        GameObject instantiatedAsset = Instantiate(GetPrefab(mapAsset.AssetId), position, orientation);
+        instantiatedAsset.name = mapAsset.AssetId;
+        sceneAssets.Add(instantiatedAsset);
 
-		Debug.Log ("Could not find correct prefab");
-		return prefabs [0];
-	}
+        Toast("Your bear's garden has been found!", 2.0f);
 
+        placeAssetButtons.SetActive(true);
+        saveAssetButton.SetActive(true);
+
+    }
+
+
+
+    //TODO: shut off in at 5. move to 5 on reload.
     private void ProgressIncrement(string progressString){
         Toast(progressString, 0.5f);
         progress++;
@@ -180,6 +145,40 @@ public class SceneControl : MonoBehaviour {
             return;
         }
         progressPanel.GetComponent<ProgressBar>().AddProgress(progress);
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadSceneAsync("LoginScene");
+    }
+
+    private GameObject GetPrefab(string prefabName)
+    {
+        Debug.Log("Looking for a " + prefabName);
+        foreach (GameObject prefab in prefabs)
+        {
+            string[] nameSplit = prefabName.Split('('); //prefabName = Type(Clone)#ID
+            if (prefab.name == nameSplit[0])
+            {
+                return prefab;
+            }
+        }
+
+        Debug.Log("Could not find correct prefab");
+        return prefabs[0];
+    }
+
+    private GameObject IsThisALoadedAsset(MapAsset asset)
+    {
+        foreach (var sceneAsset in sceneAssets)
+        {
+            if (asset.AssetId == sceneAsset.name && (asset.Position - sceneAsset.Position).magnitude < 1e-4)
+            {
+                return sceneAsset;
+            }
+        }
+
+        return null;
     }
 
 	private void Toast(string message, float time) {
