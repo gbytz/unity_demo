fa09a8e40ead8e6b48db7dd31dc586929d3a6123
Mark <stauber@stanford.edu>
2018-07-10 17:40:29 -0400

    animations

--- Assets/Helpers/AddButton.cs
+++ Assets/Helpers/AddButton.cs
@@ -9,9 +9,8 @@ public class AddButton : MonoBehaviour {
     public GameObject addPanel;
 
     public void TogglePanel(){
-        int active = (addPanel.activeSelf) ? 1 : 0;
-        print("toglle " + active);
         addPanel.SetActive(!(addPanel.activeSelf));
+        int active = (addPanel.activeSelf) ? 1 : 0;
         GetComponent<Image>().sprite = arrows[active];
     }
 }
